5f350e80866987aad40d01ceec468f6aa31f9022
shawlycode <kutinaorlando@gmail.com>
2024-06-30 00:37:37 +0000

    committed

--- controllers/blog_controllers.js
+++ controllers/blog_controllers.js
@@ -3,13 +3,26 @@ import { blogModel } from "../models/blog_models.js";
 
 //define all controllers
 //adding blogs into db using the create method
-export const add_newBlog = async (req, res, next) => {
+export const addBlog = async (req, res, next) => {
   try {
     console.log('request', req.body)
-    const addBlog = await blogModel.create(req.body)
-    res.status(200).send(addBlog)
-  } catch (error) {
+    const newBlog = await blogModel.create(req.body)
+    res.status(200).send(newBlog)
+  }
+  catch (error) {
     next(error)
   }
-
 };
+
+//get all blogs
+
+export const get_Blogs = async (req, res, next) => {
+  try {
+    console.log('request', req.body)
+    const getBlogs = await blogModel.create(req.body)
+    res.status(200).send(getBlogs)
+  } catch (error) {
+    next(error)
+
+  }
+}
