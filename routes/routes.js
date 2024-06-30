31bb9574e2295e2784eb9b8dd52801e9cf7f9023
shawlycode <kutinaorlando@gmail.com>
2024-06-30 18:50:45 +0000

    route file modified

--- routes/routes.js
+++ routes/routes.js
@@ -9,13 +9,7 @@ import {
 
 // store router in a variable
 const blogApi_route = Router();
-//define all routes
-//define endpoints
-
-//all blogs
-// blogApi_route.get('/blogs', (req, res) => {
-//   res.json('All blogs');
-// })
+//define all routes &  endpoints
 
 blogApi_route.post('/blog', addBlog);
 blogApi_route.get('/blogs', get_Blogs);
@@ -23,24 +17,5 @@ blogApi_route.get('/blog/:id', get_blogById);
 blogApi_route.patch('/blog/:id', updateBlogById);
 blogApi_route.delete('/blog/:id', deleteBlogById);
 
-//post a blog
-// blogApi_route.post('/blog', (req, res) => {
-//   res.json('My new blog');
-// })
-
-//get a single blog by ID
-// blogApi_route.get('/blog/:id', (req, res) => {
-//   res.json(`You open blog ${req.params.id}`)
-// })
-
-//update a blog by ID
-// blogApi_route.patch('/blog/:id', (req, res) => {
-//   res.json(`Blog ${req.params.id} updated`)
-// })
-
-//delete a blog by ID
-// blogApi_route.delete('/blog/:id', (req, res) => {
-//   res.json(`Blog ${req.params.id} deleted successfully`)
-// })
 
 export default blogApi_route;
